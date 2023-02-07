a295ef60e191326e0e0e86be40850d9fd8df9aed
TheLohia <mananlohia@gmail.com>
2023-02-07 19:32:27 +0800

    Testing Commit and Push

---
+++ items/function.py
@@ -0,0 +1,8 @@
+# Define a class to represent function
+import sample.level1 as l
+
+
+class FunctionNode:
+
+    def __init__(self):
+        print(l.calculate(2,3))
--- sample/level1.py
+++ sample/level1.py
@@ -1,2 +1,23 @@
-### Sample code to parse and make network
-### Level 1 involves only a few functions to keep it simpl
+# Sample code to parse and make network. Level 1 involves only a few functions to keep it simple
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+def calculate(a, b):
+    print("Sum: " + str(add(a, b)))
+    print("Difference: " + str(subtract(a, b)))
+    print("Product: " + str(multiply(a, b)))
+    print("Ratio: " + str(divide(a, b)))
